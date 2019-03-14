6baa88f84615d05d43a947d1b00fa79166f5becb
Carlos Saldana <carloss@bluetrailsoft.com>
2019-03-14 12:33:12 -0600

    Initial Commit

---
+++ DebugST/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DebugST
+//
+//  Created by iOS BTS on 3/14/19.
+//  Copyright © 2019 BlueTrailSoft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
